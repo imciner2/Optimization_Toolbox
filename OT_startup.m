e045069cfea3b2b80621d88585fe6e6fa0af1a80
Ian McInerney <I.McInerney17@imperial.ac.uk>
2017-12-12 18:16:50 +0000

    Added new MPC problem benchmark suite

--- OT_startup.m
+++ OT_startup.m
@@ -11,8 +11,15 @@ addpath( genpath(scriptDir) );
 % Remove the benchmark problems directory from the path
 rmpath( genpath([scriptDir, filesep, 'benchmarking', filesep, 'problems']) );
 
+% Readd the paths for the MPC benchmarking suite if it exists
+mpcBenchmarkingFolder = [scriptDir, filesep, 'benchmarking', filesep, 'problems', filesep, 'mpcBenchmarking'];
+if ( exist( [mpcBenchmarkingFolder, filesep, 'installBenchmarks.m'], 'file' ) )
+    addpath( genpath([mpcBenchmarkingFolder filesep 'benchmarks']) )
+    addpath( genpath([mpcBenchmarkingFolder filesep 'dataStructures']) )
+end
+
 % Remove the testbench directory from the path
 rmpath( genpath([scriptDir, filesep, 'testbenchs']) );
 
 % Remove the variables used
-clear filename scriptDir
+clear filename scriptDir mpcBenchmarkingFolder;
